47b941ef5526ed90083143013edd1b417c0619e0
IdoAbramov <59793426+IdoAbramov@users.noreply.github.com>
2022-10-19 22:36:10 +0300

    Update Base64Wrapper.cpp

--- Client/Base64Wrapper.cpp
+++ Client/Base64Wrapper.cpp
@@ -6,9 +6,8 @@ std::string Base64Wrapper::encode(const std::string& str)
 	std::string encoded;
 	
 	 // returns string "encoded" to store data, then store the encoded value of "str" string in it.
-	CryptoPP::StringSource ss(str, 
-				  true,
-				  new CryptoPP::Base64Encoder(new CryptoPP::StringSink(encoded)));
+	CryptoPP::StringSource ss(str, true, new CryptoPP::Base64Encoder(new CryptoPP::StringSink(encoded)));
+	
 	return encoded;
 }
 
@@ -17,8 +16,7 @@ std::string Base64Wrapper::decode(const std::string& str)
 	std::string decoded;
 	
 	// returns string "decoded" to store data, then store the decoded value of "str" string in it.
-	CryptoPP::StringSource ss(str, 
-				  true,
-				  new CryptoPP::Base64Decoder(new CryptoPP::StringSink(decoded)));
+	CryptoPP::StringSource ss(str, true, new CryptoPP::Base64Decoder(new CryptoPP::StringSink(decoded)));
+	
 	return decoded;
 }
