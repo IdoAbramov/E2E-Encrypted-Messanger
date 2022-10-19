c448db1233aa6e4f7fee2043f302d91349c6a1eb
IdoAbramov <59793426+IdoAbramov@users.noreply.github.com>
2022-10-19 22:38:44 +0300

    Update Base64Wrapper.h

--- Client/Base64Wrapper.h
+++ Client/Base64Wrapper.h
@@ -5,6 +5,9 @@
 class Base64Wrapper
 {
 public:
+	/*
+	* Both functions used to convert strings values to base64 value and vice versa. 
+	*/
 	static std::string encode(const std::string& str);
 	static std::string decode(const std::string& str);
 };
