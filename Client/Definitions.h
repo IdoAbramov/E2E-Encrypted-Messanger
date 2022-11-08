291cbf5c7b544b807b8f397f6790c342618fd0d7
IdoAbramov <59793426+IdoAbramov@users.noreply.github.com>
2022-11-09 01:09:04 +0200

    Update Definitions.h

--- Client/Definitions.h
+++ Client/Definitions.h
@@ -30,14 +30,14 @@
 namespace Constants {
 	const std::string CLIENT_INFO_FILE_PATH = "my.info";
 	const std::string SERVER_DATA_FILE_PATH = "server.info";
-	const uint8_t VERSION = 2;
-	const unsigned int NO_PAYLOAD = 0;
-	const unsigned int NO_MESSAGE_CONTENT = 0;
-	const unsigned int UUID_BYTES_LENGTH = 16;
-	const unsigned int PUBLIC_KEY_LENGTH = 160;
-	const unsigned int USERNAME_LENGTH = 255;
-	const unsigned int MESSAGE_MAX_LENGTH = UINT_MAX;
-	const unsigned int FILE_NAME_LEN = 12;
+	constexpr uint8_t VERSION = 2;
+	constexpr unsigned int NO_PAYLOAD = 0;
+	constexpr unsigned int NO_MESSAGE_CONTENT = 0;
+	constexpr unsigned int UUID_BYTES_LENGTH = 16;
+	constexpr unsigned int PUBLIC_KEY_LENGTH = 160;
+	constexpr unsigned int USERNAME_LENGTH = 255;
+	constexpr unsigned int MESSAGE_MAX_LENGTH = UINT_MAX;
+	constexpr unsigned int FILE_NAME_LEN = 12;
 }
 
 namespace Types {
